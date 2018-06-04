abbd9759d2cf33cefc66da414cfeffa3ca206f7b
alexandermaxs <38385660+alexandermaxs@users.noreply.github.com>
2018-06-04 15:06:38 +0300

    Add files via upload

--- src/Starter.java
+++ src/Starter.java
@@ -1,7 +1,10 @@
 import composite.Box;
 import composite.Lexeme;
+import util.RegexReader;
 import util.TextInput;
 import parser.ParagraphParser;
+
+import java.util.Properties;
 
 public class Starter {
     public static void main(String[] args){
@@ -10,6 +13,11 @@ public class Starter {
         // path from file_path.properties
         //System.out.println(textInput.getContent());
         //input text from file
+
+        RegexReader regexReader = new RegexReader();
+        regexReader.readFile("resources/regex.properties");
+        Properties props = regexReader.getProp();
+
         Box box = new Box();
         box.add(new Lexeme(textInput.getContent().toString()));
         //
@@ -17,8 +25,8 @@ public class Starter {
         /*
         SentenceParser h2 = new SentenceParser();
         WordParser h3 = new WordParser();
-        h1.setNext(h2);
-        h2.setNext(h3);
+        h1.setNextParser(h2);
+        h2.setNextParser(h3);
         h1.doChain(box);
         */
         box = h1.handle(box);
