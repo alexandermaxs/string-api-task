50ceefcb540668acbec5a23901c1c2f718fef7a9
alexandermaxs <38385660+alexandermaxs@users.noreply.github.com>
2018-06-04 15:07:22 +0300

    Add files via upload

--- src/util/RegexReader.java
+++ src/util/RegexReader.java
@@ -1,11 +1,24 @@
 package util;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class RegexReader {
-    private Properties props = new Properties();
+    private Properties prop = new Properties();
+
+    public Properties getProp() {
+        return prop;
+    }
 
     public void readFile(String filepath){
-
+        try {
+            ClassLoader loader = Thread.currentThread().getContextClassLoader();
+            InputStream stream = loader.getResourceAsStream(filepath);
+            prop.load(stream);
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
