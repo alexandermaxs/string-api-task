9350c71eaea8ea7479fb697773f6e21b8a974815
alexandermaxs <38385660+alexandermaxs@users.noreply.github.com>
2018-06-07 04:20:37 +0300

    Add files via upload

---
+++ src/com/epam/task2/Starter.java
@@ -0,0 +1,23 @@
+package com.epam.task2;
+
+import com.epam.task2.composite.Box;
+import com.epam.task2.composite.Unit;
+import com.epam.task2.parser.ParagraphParser;
+import com.epam.task2.parser.SentenceParser;
+import com.epam.task2.parser.WordParser;
+import com.epam.task2.util.TextUtility;
+
+public class Starter {
+    public static void main(String[] args){
+        TextUtility textUtility = new TextUtility();
+        Box text = new Box();
+        text.add(new Unit(textUtility.readFile("documents/text.txt")));
+        ParagraphParser h1 = new ParagraphParser();
+        SentenceParser h2 = new SentenceParser();
+        WordParser h3 = new WordParser();
+        h1.setNextParser(h2);
+        h2.setNextParser(h3);
+        text = h1.parse(text);
+        textUtility.writeToFile(text.print(), "documents/output/new_text.txt");
+    }
+}
