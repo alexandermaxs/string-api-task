9ac1d3d14aced73edeaf8069927c0e494cc53d69
alexandermaxs <38385660+alexandermaxs@users.noreply.github.com>
2018-06-15 02:24:55 +0300

    Add files via upload

--- src/com/epam/task2/Starter.java
+++ src/com/epam/task2/Starter.java
@@ -1,7 +1,7 @@
 package com.epam.task2;
 
-import com.epam.task2.composite.Box;
-import com.epam.task2.composite.Unit;
+import com.epam.task2.entity.composite.Box;
+import com.epam.task2.entity.composite.Unit;
 import com.epam.task2.parser.ParagraphParser;
 import com.epam.task2.parser.SentenceParser;
 import com.epam.task2.parser.WordParser;
@@ -10,19 +10,25 @@ import com.epam.task2.util.HitRegister;
 import com.epam.task2.util.TextUtility;
 
 public class Starter {
+    private static String textPath = "documents/text.txt";
+    private static String outputTextPath = "documents/output/new_text.txt";
+    private static String examplePath = "documents/exampleFor10And11.txt";
+    private static String result10 = "documents/output/result10.txt";
+    private static String result11 = "documents/output/result11.txt";
+
     public static void main(String[] args){
         TextUtility textUtility = new TextUtility();
         Box text = new Box();
-        text.add(new Unit(textUtility.readFile("documents/text.txt")));
+        text.add(new Unit(textUtility.readFile(textPath)));
         ParagraphParser h1 = new ParagraphParser();
         SentenceParser h2 = new SentenceParser();
         WordParser h3 = new WordParser();
         h1.setNextParser(h2);
         h2.setNextParser(h3);
         text = h1.parse(text);
-        textUtility.writeToFile(text.print(), "documents/output/new_text.txt");
-        String task11 = textUtility.readFile("documents/exampleFor10And11.txt");
-        textUtility.writeToFile(Eraser.execute(task11),"documents/output/result11.txt");
-        textUtility.writeToFile(HitRegister.info(task11), "documents/output/result10.txt");
+        textUtility.writeToFile(text.print(), outputTextPath);
+        String task11 = textUtility.readFile(examplePath);
+        textUtility.writeToFile(Eraser.execute(task11),result11);
+        textUtility.writeToFile(HitRegister.info(task11), result10);
     }
 }
